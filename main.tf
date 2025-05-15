0015c64e21ecaa0c5ec2e8be811c18e2597d0a68
bsugar01 <s2172274@students.ncl-coll.ac.uk>
2025-05-15 13:42:05 +0100

    checkj

--- main.tf
+++ main.tf
@@ -7,42 +7,39 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-# Generate unique suffix for storage account name
 resource "random_id" "unique" {
   byte_length = 4
 }
 
-# Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "cafe-website-rg"
   location = "UK South"
 }
 
-# Create a storage account with static website enabled
 resource "azurerm_storage_account" "static_site" {
   name                     = "cafeweb${random_id.unique.hex}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  static_website {
-    index_document     = "index.html"
-    error_404_document = "404.html"
-  }
+  allow_blob_public_access = true
 
   tags = {
     environment = "production"
   }
 }
 
-# Output the website URL
+resource "azurerm_storage_account_static_website" "website" {
+  storage_account_id = azurerm_storage_account.static_site.id
+  index_document     = "index.html"
+  error_404_document = "404.html"
+}
+
 output "static_website_url" {
-  value       = azurerm_storage_account.static_site.primary_web_endpoint
+  value       = azurerm_storage_account_static_website.website.primary_web_endpoint
   description = "URL to access your deployed static website"
 }
 
-# Declare input variables
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
