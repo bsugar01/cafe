49a307a183d790ea10c187ab5d88eecb9bb34dab
bsugar01 <s2172274@students.ncl-coll.ac.uk>
2025-05-16 09:10:38 +0100

    cg

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.20.0"
+    }
+  }
+}
+
 provider "docker" {}
 
 resource "docker_image" "nginx" {
